31289d96fd0225860ddac1965b4ee625c124488d
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-06-20 21:12:35 +0200

    fixes passing on of *args to .joinpath() in get_project_root()

--- typhon/utils.py
+++ typhon/utils.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 def get_project_root(*args) -> Path:
     """Returns project root folder."""
-    return Path(__file__).parent.parent.joinpath()
+    return Path(__file__).parent.parent.joinpath(*args)
