0abf90a49c0d217b1965da6553c871f2b1c2c8a3
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-06-20 22:14:27 +0200

    cleanup

--- typhon/Converter.py
+++ typhon/Converter.py
@@ -1,6 +1,8 @@
 import shutil
 import pydub
 from pathlib import Path
+from typing import Union, Optional
+
 
 class Converter:
     pass
@@ -11,17 +13,18 @@ class CopyConverter:
     def __init__(self) -> None:
         pass
 
-    def run(self, src, dst):
+    def run(self, src: Union[Path, str], dst: Union[Path, str]):
         shutil.copy(src, dst)
 
 
-
 class OggConverter:
-    """Dummy converter that only copies a file without any processing"""
-    def __init__(self, bitrate: str = "192") -> None:
+    """Convert audio files to ogg vorbis"""
+    def __init__(self, bitrate: str = "192k") -> None:
         self.bitrate = bitrate
         pass
 
-    def run(self, src: Path, dst: Path):
+    def run(self, src: Union[Path, str], dst: Union[Path, str]) -> None:
+        src = Path(src).resolve()
+        dst = Path(dst).resolve()
         x = pydub.AudioSegment.from_file(src)
-        x.export(dst, format="ogg")
+        x.export(dst, format="ogg", bitrate=self.bitrate)
--- typhon/utils.py
+++ typhon/utils.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 def get_project_root(*args) -> Path:
     """Returns project root folder."""
     return Path(__file__).parent.parent.joinpath(*args)
