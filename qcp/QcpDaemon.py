26197ed00a254485954a00044e87f8cb72d9b32d
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-06-27 11:34:20 +0200

    implements request and response object for

--- qcp/QcpDaemon.py
+++ qcp/QcpDaemon.py
@@ -1,31 +1,54 @@
-import socketserver
-import socket
+import struct
+import json
+from typing import Dict
 
 
-class QcpDaemon(socketserver.BaseRequestHandler):
-    """
-    The request handler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
-
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
-        self.sh
+PREHEADER_LEN: int = 2
 
 
-if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
+class Request:
+    """Container for requests sent to the qcp daemon"""
+    def __init__(self, body) -> None:
+        assert isinstance(body, dict)
+        self.body = body
 
-    # Create the server, binding to localhost on port 9999
-    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
-        # Activate the server; this will keep running until you
-        # interrupt the program with Ctrl-C
-        server.serve_forever()
+    def encode(self) -> bytes:
+        if isinstance(self.body, dict):
+            body: bytes = bytes(json.dumps(self.body), "utf-8")
+            header: Dict = {
+                "content-length": len(body),
+                "content-type": "text/json"
+            }
+            header: bytes = bytes(json.dumps(header), "utf-8")
+            header_len: bytes = struct.pack("!H", len(header))  # network-endianess, unsigned long integer (4 bytes)
+
+            return header_len + header + body
+
+
+class Response:
+    """Container for responses from the qcp daemon"""
+
+    def __init__(self, raw) -> None:
+        assert isinstance(raw, bytes)
+        self.raw: bytes = raw
+
+    @property
+    def header_len(self) -> int:
+        return int(struct.unpack("!H", self.raw[:PREHEADER_LEN])[0])
+
+    @property
+    def _header(self) -> bytes:
+        return self.raw[PREHEADER_LEN:(self.header_len + PREHEADER_LEN)]
+
+    @property
+    def header(self) -> Dict:
+        return json.loads(self._header.decode("utf-8"))
+
+    @property
+    def _body(self) -> bytes:
+        start = PREHEADER_LEN + self.header_len
+        return self.raw[start:start + self.header["content-length"]]
+
+    @property
+    def body(self) -> Dict:
+        return json.loads(self._body.decode("utf-8"))
