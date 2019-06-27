053cfb1df6e9144a29045f51be5356e8ce9179a9
Stefan Fleck <stefan.b.fleck@gmail.com>
2019-06-27 07:57:36 +0200

    more experiments with client/server code

--- qcp.py
+++ qcp.py
@@ -1,20 +1,53 @@
 import socket
 import subprocess
 import sys
+import logging
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lg = logging.getLogger(__name__)
+logging.basicConfig(level="DEBUG")
 
-try:
-    s.connect((socket.gethostname(), 9393))
-    print("connected to existing server")
-except:
-    print("Launching server")
-    subprocess.Popen((sys.executable, 'qcpd.py'))
-    print('connecting')
-    s.connect((socket.gethostname(), 9393))
-    print("connected")
+HEADERSIZE = 10
 
-s.send(bytes("hoelk2", "utf-8"))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("127.0.0.1", 9393))
+print("connected to existing server")
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+cmd = "command blah blah"
+cmd = f'{len(cmd):<{HEADERSIZE}}{cmd}'
+
+print(cmd)
+client.send(bytes(cmd, "utf-8"))
+
+while True:
+    msg = ""
+    is_new_msg = True
+
+    while True:
+        msg_part = client.recv(16)
+
+        if is_new_msg:
+            is_new_msg = False
+            lg.debug(f"received {msg_part}")
+            len_msg = int(msg_part[:HEADERSIZE])
+            lg.debug(f'new message length: {len_msg}')
+
+        msg += msg_part.decode("utf-8")
+        lg.debug(f'msg part {msg_part}')
+
+        if len(msg) - HEADERSIZE == len_msg:
+            lg.debug("full msg received")
+            lg.debug(msg[HEADERSIZE:])
+            msg = ""
+            is_new_msg = True
+            client.close()
+
+
+# try:
+#     s.connect((socket.gethostname(), 9393))
+#     print("connected to existing server")
+# except:
+#     print("Launching server")
+#     subprocess.Popen((sys.executable, 'qcpd.py'))
+#     print('connecting')
+#     s.connect((socket.gethostname(), 9393))
+#     print("connected")
--- qcpd.py
+++ qcpd.py
@@ -1,15 +1,23 @@
 import socket
+import logging
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # ADDRESS_FAMILY: INTERNET (ip4), tcp
-s.bind((socket.gethostname(), 9393))
-s.listen(10)
+lg = logging.getLogger(__name__)
+logging.basicConfig(level="DEBUG")
 
+HEADERSIZE = 10
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # ADDRESS_FAMILY: INTERNET (ip4), tcp
+server.bind(("127.0.0.1", 9393))
+server.listen(10)
+lg.info("Started qcp daemon")
 
 while True:
-    conn, address = s.accept()
-    print(f'Connection from {address} has been established!')
-    cmd = conn.recv(1024)
+    client, address = server.accept()
+    lg.info(f'Established connection with client {address}')
+    cmd = client.recv(1024)
 
-    conn.send(f'Welcome {cmd} {address}', 'utf-8')
-    conn.close()
+    reply = f'{len(cmd):<{HEADERSIZE}}{cmd}' # add fixed length header that includes length of the message to the msg
+    lg.debug("Sending reply: " + reply)
+    client.send(bytes(reply, 'utf-8'))
+
+
 
