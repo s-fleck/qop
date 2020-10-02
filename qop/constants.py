a2bd3968e380bb0f486f3bec54c470867d625160
Stefan Fleck <stefan.b.fleck@gmail.com>
2020-10-02 14:03:24 +0200

    doc

--- qop/constants.py
+++ qop/constants.py
@@ -1,3 +1,9 @@
+"""
+This module defines global constants and enums. The numeric values of the integer enums 
+are used to represent the respective value when serializing qop objects into a TaskQueue
+sqlite3 database.
+"""
+
 from enum import IntEnum
 from typing import Union
 from pathlib import Path
@@ -6,6 +12,11 @@ Pathish = Union[Path, str]
 
 
 class Status(IntEnum):
+    """
+    Status codes are used by
+      - TaskQueue to represent the status of a task in the queue, and
+      - StatusMessage to indicate whether a command sent to the server was successful or not
+    """
     FAIL = -1
     PENDING = 0
     OK = 1
@@ -14,12 +25,14 @@ class Status(IntEnum):
 
 
 class ConverterType(IntEnum):
+    """Types of converters; used when serializing converters to json"""
     COPY = 0
     MP3 = 1
     OGG = 2
 
 
 class Command(IntEnum):
+    """Commands codes that can be sent to the daemon via a CommandMessage"""
     DAEMON_START = 101
     DAEMON_STOP = 102
     DAEMON_IS_ACTIVE = 103
@@ -37,6 +50,7 @@ class Command(IntEnum):
 
 
 class PayloadClass(IntEnum):
+    """Types of Payloads that can be part of CommandMessage or StatusMessage"""
     VALUE = 1  # a single value {"value": <value>}
     TASK = 2
     QUEUE_PROGRESS = 3
@@ -45,6 +59,7 @@ class PayloadClass(IntEnum):
 
 
 class TaskType(IntEnum):
+    """Types of Tasks. Corresponds to the subclasses of tasks.Task"""
     ECHO = 1
     FILE = 2
     DELETE = 3
